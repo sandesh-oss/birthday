61d95a6b153818d9d2cac75f60e475bbbe1775b7
sandesh-oss <sandeshpatel020@gmail.com>
2025-04-10 17:32:07 +0530

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,19 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+//import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import {HashRouter, Routes, Route} from "react-router-dom";
 import BirthdayGreeting from './components/BirthdayGreeting';
 import Surprise from './components/Surprise';
 import Message from './components/Message';
 
 function App() {
   return (
-    <Router>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<BirthdayGreeting />} />
         <Route path="/surprise" element={<Surprise />} />
         <Route path="/message" element={<Message />} />
       </Routes>
-    </Router>
+    </HashRouter>
   );
 }
 
