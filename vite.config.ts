b1c1c345a8e11ec066d25a5baa52e7d85963e4a1
sandesh-oss <sandeshpatel020@gmail.com>
2025-04-12 17:03:57 +0530

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,9 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/birthday/",
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
+  base: "/birthday",
 });
