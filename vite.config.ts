8c59cc27fce3fec47ace434160d7f9b6cc242a55
sandesh-oss <sandeshpatel020@gmail.com>
2025-04-19 09:50:57 +0530

    sfinal

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,15 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/birthday/',
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
+  base: './',
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+    rollupOptions: {
+      output: {
+        assetFileNames: 'assets/[name].[ext]',
+    }
+  }
+ }
 });
